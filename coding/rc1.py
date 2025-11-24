c9e36676c343c10bfcfd49a2b3b4216ede1beb7b
mingyuzhou <228632512@qq.com>
2025-11-24 16:00:30 +0800

    开始做毕设

--- coding/rc1.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.layers import StringLookup
-data = [["a", "c", "d"], ["d", "z", "b"]]
-layer = StringLookup()
-layer.adapt(data)
-
-print(layer.get_vocabulary())
