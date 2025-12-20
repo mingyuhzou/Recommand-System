dd5274d8afba3ae078321695a2983454b7f074d3
mingyuhzou <mingyuhzou@gmail.com>
2025-12-21 02:02:35 +0800

    finish rak model n

--- 项目/OTTO/code/demo.py
+++ 项目/OTTO/code/demo.py
@@ -1,9 +1,3 @@
-import cudf
-train = cudf.read_parquet('../data/processData/train.parquet')
-train=cudf.read_parquet('../data/processData/train.parquet')
-test=cudf.read_parquet('../data/processData/test.parquet')
-train_pairs = cudf.concat([train, test])[['session', 'aid']]
-del train, test
 
-train_pairs['aid_next'] = train_pairs.groupby('session').aid.shift(-1)
-train_pairs = train_pairs[['aid', 'aid_next']].dropna().reset_index(drop=True)
+
+
